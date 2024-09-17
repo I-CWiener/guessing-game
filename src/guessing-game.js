bb5f5580786b8236762b9cccb1c0e1a5dfd5c0f3
I-CWiener <150772092+I-CWiener@users.noreply.github.com>
2024-09-17 11:37:58 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,8 +1,8 @@
 class GuessingGame {
     constructor() {}
-        this.min = 0;
-        this.max = 0;
-        this.ges = 0;
+        this.min = null;
+        this.max = null;
+        this.ges = null;
     setRange(min, max) {
         this.min = min;
         this.max = max;
